6f7b1983d37c190f14cfbd9ba607632d68c13b2d
kubodread <202198206+kubodread@users.noreply.github.com>
2025-04-19 16:12:05 -0600

    actualizando deshabilitando feed en index

--- index.js
+++ index.js
@@ -7,3 +7,28 @@ window.onload = function () {
     
  };
  
+
+ console.log ('Hola! ¡Bienvenid@s! \nProductos para tus plantas \nKubiotec! bio soluciones ');
+
+ const salc = 
+    [{
+    producto:'Ultrabal' , 
+    precio : '$100' , 
+    presentacion : '500ml' , 
+    uso: 'Abono para crecimiento y floración, \n con microorganismos benéficos, alto en macro elementos, ac. fúlvicos y húmicos y micro elementos. '
+    },{
+        producto:'sulfocaldo', 
+        precio: '$80', 
+        presentacion: '330ml',
+        uso: 'insecticida, contra hongos, bacterias e insectos, aporta nutrientes como calcio, magnesio, potasio y azufre.'
+
+    },{
+        producto:'Alfin Jabón',
+        precio: '$30',
+        presentacion: "En sobre para 1 a 2 litros",
+        uso: 'insecticida'
+    }
+
+    ];
+
+console.table(salc);
