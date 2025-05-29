3b016d5e37291e58f3376d301708be5a282dbc16
kubodread <202198206+kubodread@users.noreply.github.com>
2025-05-28 18:14:36 -0600

    Se genera compostaje.html como pwa para uso offline

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+    '',
+    './compostaje.html',
+    './csscomposta.css',
+    './app.js',
+    './manifest.json',
+    './preload.js',
+    './images/icon-192x192.png',
+    './images/icon-512x512.png'
+    // 'https://composta-control.vercel.app/'
+    // No se recomienda cachear la URL del iframe aquí
+    // ya que el service worker del iframe se encargará de su propio caché.
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
